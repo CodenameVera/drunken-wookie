25447ac9ddd10dbaa1b283ecb0782428fe506038
Chouser33 <chouser@monetate.com>
2015-01-12 11:30:11 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
-#! /usr/bin/env python2.7
-try:
-    from setuptools import setup, find_packages
-except ImportError as e:
-    import ez_setup
-    ez_setup.use_setuptools()
-finally:
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
-setup(name="ircdd",
-      version="alpha",
-      description="Distributed IRC Daemon",
-      url="github.com/kzvezdarov/ircdd",
-      )
+
+setup(
+    name='readthedocs',
+    version="1.0",
+    description='A documentation hosting website',
+    #long_description=readme,
+    author='Eric Holscher, Charles Leifer, Bobby Grace',
+    author_email='eric@ericholscher.com',
+    url='http://readthedocs.org',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
