5efbf6f9aa4421cab8d58779a3e64dc840462149
Chouser33 <chouser@monetate.com>
2015-01-12 11:31:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
     ],
 )
